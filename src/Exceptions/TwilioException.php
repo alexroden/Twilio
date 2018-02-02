a9e34e71487a3bfaba44bc89d841468f414b3715
Alex Roden <alex.br@bluebaytravel.co.uk>
2018-02-02 23:58:07 +0000

    Apply fixes from StyleCI

--- src/Exceptions/TwilioException.php
+++ src/Exceptions/TwilioException.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Twilio.
+ *
+ * (c) Alex Broom-Roden <b.r_alex@hotmail.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace ABR\Twilio\Exceptions;
 
 use Exception;
@@ -13,7 +22,7 @@ class TwilioException extends Exception implements TwilioExceptionInterface
 {
     /**
      * Create a new twilio exception instance.
-     * 
+     *
      * @param string         $message
      * @param [int           $code
      * @param Exception|null $previous
