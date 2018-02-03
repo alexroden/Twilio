429efd4011ac7b23fe0d09b40b0bd534ecdb161b
Alex Roden <alex.br@bluebaytravel.co.uk>
2018-02-03 13:07:46 +0000

    Apply fixes from StyleCI

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -1,22 +1,31 @@
 <?php
 
+/*
+ * This file is part of Laravel Twilio.
+ *
+ * (c) Alex Broom-Roden <b.r_alex@hotmail.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace ABR\Tests\Twilio;
 
 use ABR\Twilio\TwilioServiceProvider;
 use GrahamCampbell\TestBench\AbstractPackageTestCase;
 
 /**
- * This is the abstract test class. 
+ * This is the abstract test class.
  *
  * @author Alex Broom-Roden <b.r_alex@hotmail.co.uk>
  */
 abstract class AbstractTestCase extends AbstractPackageTestCase
 {
     /**
-     * Get the service provider class. 
-     * 
+     * Get the service provider class.
+     *
      * @param \Illuminate\Contracts\Foundation\Application $app
-     * 
+     *
      * @return string
      */
     protected function getServiceProviderClass($app)
--- tests/Functional/TwilioTest.php
+++ tests/Functional/TwilioTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Twilio.
+ *
+ * (c) Alex Broom-Roden <b.r_alex@hotmail.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace ABR\Tests\Twilio\Funcational;
 
 use ABR\Tests\Twilio\FunctionalTestCase;
